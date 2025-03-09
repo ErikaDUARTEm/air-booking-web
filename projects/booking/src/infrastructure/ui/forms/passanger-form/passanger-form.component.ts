599ec371bf95299c2a5250d07c1e563aee2def74
Lucas Mazzarino Capurro <mazzanga1212@gmail.com>
2025-03-09 17:02:40 -0300

    feature(passanger)Create-passanger-form

---
+++ projects/booking/src/infrastructure/ui/forms/passanger-form/passanger-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-passanger-form',
+  imports: [],
+  templateUrl: './passanger-form.component.html',
+  styleUrl: './passanger-form.component.css'
+})
+export class PassangerFormComponent {
+
+}
