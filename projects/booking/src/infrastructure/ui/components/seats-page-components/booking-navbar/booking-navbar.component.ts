15adeb88e3126345a087d05f480590e4e820b97a
J-Ciro <jciro.1984@gmail.com>
2025-03-10 10:45:41 -0500

    feature(seats): created the seats page navbar mock

---
+++ projects/booking/src/infrastructure/ui/components/seats-page-components/booking-navbar/booking-navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-booking-navbar',
+  imports: [],
+  templateUrl: './booking-navbar.component.html',
+  styleUrl: './booking-navbar.component.scss'
+})
+export class BookingNevbarComponent {
+
+}
