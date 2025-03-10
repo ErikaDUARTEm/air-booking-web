f549d65a1e7fa7c3cab2bca91fdb77335fa50cd8
J-Ciro <jciro.1984@gmail.com>
2025-03-10 10:47:59 -0500

    feature(seats): created the base seats details component

---
+++ projects/booking/src/infrastructure/ui/components/seats-page-components/booking-seats-details/booking-seats-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-booking-seats-details',
+  imports: [],
+  templateUrl: './booking-seats-details.component.html',
+  styleUrl: './booking-seats-details.component.css'
+})
+export class BookingSeatsDetailsComponent {
+
+}
