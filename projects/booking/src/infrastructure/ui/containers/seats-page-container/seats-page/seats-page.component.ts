4fbe7a7a48c03fbfebb88d3eb2bcaddc6603042b
J-Ciro <jciro.1984@gmail.com>
2025-03-10 10:51:12 -0500

    feature(seats): created the base page for the seats selection

---
+++ projects/booking/src/infrastructure/ui/containers/seats-page-container/seats-page/seats-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-seats-page',
+  imports: [],
+  templateUrl: './seats-page.component.html',
+  styleUrl: './seats-page.component.scss'
+})
+export class SeatsPageComponent {
+
+}
