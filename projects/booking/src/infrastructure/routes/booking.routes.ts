c386468b10f0186a80c57d58cbd64ef8c2091b06
Lucas Mazzarino Capurro <mazzanga1212@gmail.com>
2025-03-13 16:11:33 -0300

    feature(passenger): add router

--- projects/booking/src/infrastructure/routes/booking.routes.ts
+++ projects/booking/src/infrastructure/routes/booking.routes.ts
@@ -2,6 +2,9 @@ import { Routes } from "@angular/router";
 import { ReservationLayoutComponent } from "../ui/layouts/reservation-layout/reservation-layout.component";
 import { PaymentMethodContainerComponent } from "../ui/containers/payment-method-container/payment-method-container.component";
 import { SeatsPageComponent } from "../ui/containers/seats-page-container/seats-page/seats-page.component";
+import { Component } from "@angular/core";
+import { FormLayoutComponent } from "../ui/layouts/form-layout/form-layout.component";
+import { PassangerContainerComponent } from "../ui/containers/passanger-container/passanger-container.component";
 
 export const bookingRoutes: Routes = [
   {
@@ -10,12 +13,29 @@ export const bookingRoutes: Routes = [
     children: [
       {
         path: 'payment',
-        component: PaymentMethodContainerComponent
+        component: FormLayoutComponent,
+        children: [
+          {
+            path: '',
+            component: PaymentMethodContainerComponent,
+          },
+        ],
       },
 
       {
-        path: '',
-        component: SeatsPageComponent
+        path: 'seats',
+        component: SeatsPageComponent,
+      },
+
+      {
+        path: 'passanger',
+        component: FormLayoutComponent,
+        children: [
+          {
+            path: '',
+            component: PassangerContainerComponent,
+          },
+        ],
       },
     ],
   },
