299b8a0b159a79236c6e680ca7da829dc543a5e2
erika.duarte <erika.duarte.mora@gmail.com>
2025-03-13 18:17:47 -0500

    feat(booking): merge authentication

--- projects/authentication/src/infrastructure/ui/forms/user-register/user-register.component.ts
+++ projects/authentication/src/infrastructure/ui/forms/user-register/user-register.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject, Output, EventEmitter } from '@angular/core';
+import { FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import { IUser } from '../../../../domain/model/user.model';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'lib-user-register',
-  imports: [],
+  imports: [ReactiveFormsModule,NgIf],
   templateUrl: './user-register.component.html',
   styleUrl: './user-register.component.scss'
 })
+
 export class UserRegisterComponent {
+  private formBuilder = inject(FormBuilder);
+
+  @Output() onSubmit = new EventEmitter<IUser>();
+
+  public form = this.formBuilder.group({
+    name: ['', [Validators.required, Validators.pattern(/^[A-Za-zÁÉÍÓÚáéíóúñÑ\s]+$/)]], 
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(5)]],
+    documentId: ['', Validators.required],
+    phoneNumber: ['', [Validators.required, Validators.pattern(/^\+?\d+$/)]], 
+    nacionality: ['', [Validators.required, Validators.pattern(/^[A-Za-zÁÉÍÓÚáéíóúñÑ\s]+$/)]] 
+  });
+  
+  Submit(event: Event) {
+    event.preventDefault();
+    if (!this.form.valid){
+      console.log("formulario invalido");
+    }else{
+      this.onSubmit.emit(this.form.getRawValue());
+    }
+  }
 
 }
